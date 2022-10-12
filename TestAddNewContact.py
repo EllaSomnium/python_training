f8753f12b98be5d81f3018960dd7480f9d235c0e
EllaSomnium <ella.mukh@ya.ru>
2022-10-12 06:49:38 +0300

    Добавлен класс для контакта Переменные для xpath дня и месяца не исправлены

--- TestAddNewContact.py
+++ TestAddNewContact.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import unittest
+from contact_info import Contact
 
 class TestAddNewContact(unittest.TestCase):
     def setUp(self):
@@ -15,8 +16,8 @@ class TestAddNewContact(unittest.TestCase):
         wd = self.wd
         self.open_homepage(wd)
         self.login(wd, user="admin", password="secret")
-        self.add_new_contact(wd, firstname="Ella", lastname="Sova", nickname="Somnium", company_name="Lenvendo", address="City, street, house", home_number="1234567",
-                             mobile_number="+79818486206", work_number="-", email1="ella.mukh@ya.ru", email2="ella.somnium@gmail.com", bday="19", bmonth="May", byear="1992")
+        self.add_new_contact(wd, Contact(firstname="Ella", lastname="Sova", nickname="Somnium", company_name="Lenvendo", address="City, street, house", home_number="1234567",
+                             mobile_number="+79818486206", work_number="-", email1="ella.mukh@ya.ru", email2="ella.somnium@gmail.com", bday="19", bmonth="May", byear="1992"))
         self.return_to_homepage(wd)
         self.logout(wd)
 
@@ -26,61 +27,60 @@ class TestAddNewContact(unittest.TestCase):
     def return_to_homepage(self, wd):
         wd.find_element_by_link_text("home").click()
 
-    def add_new_contact(self, wd, firstname, lastname, nickname, company_name, address, home_number, mobile_number,
-                        work_number, email1, email2, bday, bmonth, byear):
+    def add_new_contact(self, wd, contact_info):
         #Add firstname
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(contact_info.firstname)
         # Add lastname
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(contact_info.lastname)
         # Add nickname
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(nickname)
+        wd.find_element_by_name("nickname").send_keys(contact_info.nickname)
         # Add company
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(company_name)
+        wd.find_element_by_name("company").send_keys(contact_info.company_name)
         # Add address
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("address").send_keys(contact_info.address)
         # Add  telephone home number
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(home_number)
+        wd.find_element_by_name("home").send_keys(contact_info.home_number)
         # Add telephone mobile number
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(mobile_number)
+        wd.find_element_by_name("mobile").send_keys(contact_info.mobile_number)
         # Add work telephone number
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(work_number)
+        wd.find_element_by_name("work").send_keys(contact_info.work_number)
         # Add first email
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(email1)
+        wd.find_element_by_name("email").send_keys(contact_info.email1)
         # Add second email
         wd.find_element_by_name("email2").click()
         wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(email2)
+        wd.find_element_by_name("email2").send_keys(contact_info.email2)
         # Add birthday date
         wd.find_element_by_name("bday").click()
         # Add birthday date day
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(bday)
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact_info.bday)
         wd.find_element_by_xpath("//option[@value='19']").click()
         # Add birthday date month
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(bmonth)
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact_info.bmonth)
         wd.find_element_by_xpath("//option[@value='May']").click()
         # Add birthday date year
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(byear)
+        wd.find_element_by_name("byear").send_keys(contact_info.byear)
         # input
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
---
+++ contact_info.py
@@ -0,0 +1,17 @@
+class Contact:
+
+    def __init__(self, firstname, lastname, nickname, company_name, address, home_number, mobile_number,
+                        work_number, email1, email2, bday, bmonth, byear):
+        self.firstname = firstname,
+        self.lastname = lastname,
+        self.nickname = nickname,
+        self.company_name = company_name,
+        self.address = address,
+        self.home_number = home_number,
+        self.mobile_number = mobile_number,
+        self.work_number = work_number,
+        self.email1 = email1,
+        self.email2 = email2,
+        self.bday = bday,
+        self.bmonth = bmonth,
+        self.byear = byear
