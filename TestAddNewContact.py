7eab58f060547323cc83afba90fec5260dbe24af
EllaSomnium <ella.mukh@ya.ru>
2022-10-29 03:02:39 +0300

    добавлен класс ApplicationC для теста TestAddNewContact, все методы убраны в класс ApplicationC. добавлена фикстура

--- TestAddNewContact.py
+++ TestAddNewContact.py
@@ -1,102 +1,18 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-import unittest
+import pytest
 from contact_info import Contact
+from application1 import ApplicationC
 
-class TestAddNewContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-    
-    def test_add_new_contact(self):
-        wd = self.wd
-        self.open_homepage(wd)
-        self.login(wd, user="admin", password="secret")
-        self.add_new_contact(wd, Contact(firstname="Ella", lastname="Sova", nickname="Somnium", company_name="Lenvendo", address="City, street, house", home_number="1234567",
-                             mobile_number="+79818486206", work_number="-", email1="ella.mukh@ya.ru", email2="ella.somnium@gmail.com", bday="19", bmonth="May", byear="1992"))
-        self.return_to_homepage(wd)
-        self.logout(wd)
 
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
+@pytest.fixture
+def appl(request):
+    fixture = ApplicationC()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def return_to_homepage(self, wd):
-        wd.find_element_by_link_text("home").click()
 
-    def add_new_contact(self, wd, contact_info):
-        #Add firstname
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact_info.firstname)
-        # Add lastname
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact_info.lastname)
-        # Add nickname
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact_info.nickname)
-        # Add company
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact_info.company_name)
-        # Add address
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact_info.address)
-        # Add  telephone home number
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact_info.home_number)
-        # Add telephone mobile number
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact_info.mobile_number)
-        # Add work telephone number
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact_info.work_number)
-        # Add first email
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact_info.email1)
-        # Add second email
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact_info.email2)
-        # Add birthday date
-        wd.find_element_by_name("bday").click()
-        # Add birthday date day
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact_info.bday)
-        # Add birthday date month
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact_info.bmonth)
-        # Add birthday date year
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact_info.byear)
-        # input
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def login(self, wd, user, password):
-        # login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(user)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_homepage(self, wd):
-        wd.get("http://localhost/addressbook/edit.php")
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_new_contact(appl):
+    appl.login(user="admin", password="secret")
+    appl.add_new_contact(Contact(firstname="Ella", lastname="Sova", nickname="Somnium", company_name="Lenvendo", address="City, street, house", home_number="1234567", mobile_number="+79818486206", work_number="-",
+    email1="ella.mukh@ya.ru", email2="ella.somnium@gmail.com", bday="19", bmonth="May", byear="1992"))
+    appl.logout()
